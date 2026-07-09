39755515c29ee5a5b72f60d333a338d9e5946705
dav55566666 <hovsepyandavid0@gmail.com>
2026-07-09 11:11:34 +0400

    update

--- src/core/pages/ProjectsSegment/ProjectsSegment.tsx
+++ src/core/pages/ProjectsSegment/ProjectsSegment.tsx
@@ -1,6 +1,6 @@
 import { Navigate, useParams } from "react-router-dom";
 
-import { OurProjects, SiteFooter } from "../../components";
+import { OurProjects } from "../../components";
 import {
   DEFAULT_PROJECT_CATEGORY,
   isProjectCategory,
